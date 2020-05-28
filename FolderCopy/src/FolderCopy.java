7e2e63f01b4a47286a27d5b914b346bdee3ea0c4
Vadim <vsverzhinskiy@gmail.com>
2020-05-28 21:17:30 +0300

    FolderCopy 9.7, copying with subfolders (overwrite if needed)

--- FolderCopy/src/FolderCopy.java
+++ FolderCopy/src/FolderCopy.java
@@ -1,34 +1,36 @@
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
 
 public class FolderCopy {
+
+    private final static boolean overwrite = true;
+    private final static Path source = Paths.get("C:\\Users\\User\\Desktop\\Свет_и_очаг");
+    private final static Path target = Paths.get("C:\\Users\\User\\Desktop\\Свет_и_очаг_2");
+
     public static void main(String[] args) {
-        String pathCopyFrom = "C:\\Users\\User\\Desktop\\Свет_и_очаг";
-        String pathCopyTo = "C:\\Users\\User\\Desktop\\Свет_и_очаг_2\\";
-
-        File folder = new File(pathCopyTo);
-        folder.mkdir();
-
-        Path folderCopyFrom = Paths.get(pathCopyFrom);
-
-        Path folderCopyTo =Paths.get(pathCopyTo);
-
-        try {
-        Files.walkFileTree(folderCopyFrom, new SimpleFileVisitor<Path>() {
-            @Override
-            public FileVisitResult visitFile(Path path, BasicFileAttributes attrs) throws IOException {
-                FileInputStream is = new FileInputStream(path.toString());
-                File file = new File(pathCopyTo + path.getFileName().toString());
-                Files.copy(is, file.toPath());
-                return FileVisitResult.CONTINUE;
-            }
-        });
+            copyFile(source, target, overwrite);
     }
-        catch (IOException e){
-            e.printStackTrace();
+        private static void copyFile(Path source, Path target, boolean overwrite) {
+            try {
+                Files.walkFileTree(source, new SimpleFileVisitor<>() {
+                    public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                        Path newDir = target.resolve(source.relativize(dir));
+                        if (Files.notExists(newDir)) Files.copy(dir, newDir);
+                            return FileVisitResult.CONTINUE;
+                    }
+
+                    @Override
+                    public FileVisitResult visitFile(Path path, BasicFileAttributes attrs) throws IOException {
+                        CopyOption[] options = (overwrite) ? new CopyOption[]{REPLACE_EXISTING} : new CopyOption[]{};
+                        Files.copy(path, target.resolve(source.relativize(path)), options);
+                            return FileVisitResult.CONTINUE;
+                    }
+                });
+
+            } catch (IOException e) {
+                System.out.format("Unable to copy %s - %s%n", source, e.getClass().getSimpleName());
+            }
         }
     }
-}
