96ba6687a0fd465d032de9d0d205ca1677fafd32
Vadim <vsverzhinskiy@gmail.com>
2020-06-02 00:02:17 +0300

    BankStatement 9.9, initial commit

---
+++ BankStatement/src/Loader.java
@@ -0,0 +1,40 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Loader {
+    final static Path bankStatement = Paths.get("data/movementList.csv");
+    static List<String> operation = new ArrayList<>();
+    static List<BankTransaction> transaction = new ArrayList<>();
+    private static String[] transactionsInfo;
+
+    public static void main(String[] args) {
+        try {
+            operation = Files.readAllLines(bankStatement);
+            for (int i = 1; i < operation.size(); i++) {
+                String noQuotes = operation.get(i).replaceAll("[\"]([\\d]+)[,]([\\d]+)[\"]", "$1.$2");
+                transactionsInfo = noQuotes.split("[,]");
+
+                if (transactionsInfo.length != 8) {
+                    System.out.println("Error!");
+                }
+
+                Double income = Double.parseDouble(transactionsInfo[6]);
+                Double expenditure = Double.parseDouble(transactionsInfo[7]);
+                String itemOfExpenditure = transactionsInfo[5]
+                        .replaceAll("[\\d\\D]+\\b[\\\\/]([^\\s>]+)[\\d\\D]+", "$1");
+
+                transaction.add(new BankTransaction(income, expenditure, itemOfExpenditure));
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        for (BankTransaction bank: transaction){
+            System.out.println(bank.getItemOfExpenditure() + " " + bank.getIncome() + "\t" + bank.getExpenditure());
+        }
+    }
+}
