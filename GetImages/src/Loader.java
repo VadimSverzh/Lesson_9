791e2be2cd87ad11b16605240cfe1ff542434cf7
Vadim <vsverzhinskiy@gmail.com>
2020-06-21 18:30:21 +0300

    GetImages 9.12, different methods

--- GetImages/src/Loader.java
+++ GetImages/src/Loader.java
@@ -1,4 +1,3 @@
-import com.sun.source.tree.ContinueTree;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -10,56 +9,72 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
 import java.nio.file.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Loader {
     private static final String URL = "http://lenta.ru/";
     private static final String TAG_ATTRIBUTE = "src";
     private static final String TAG = "img";
-    private static final String PATH_STRING = "C:\\Users\\User\\Desktop\\Java\\Skillbox\\Lesson_9\\GetImages\\images";
+    private static final Path PATH_STRING =Paths.get("C:\\Users\\User\\Desktop\\Java\\Skillbox\\Lesson_9\\GetImages\\images");
     private static boolean OVERWRITE = true;
 
     public static void main(String[] args) {
 
-        StringBuilder sb = new StringBuilder("Скаченные файлы:\n");
-        File images = new File(PATH_STRING);
+        File images = new File(String.valueOf(PATH_STRING));
         images.mkdir();
 
+        WebsiteInfo info = new WebsiteInfo(URL, TAG, TAG_ATTRIBUTE);
+
         try {
-            Document doc = Jsoup.connect(URL).get();
-            Elements imgs = doc.select(TAG);
+            Document doc = parseHtml(URL);
+            Map<URI, URI> imagesLinks = getImagesLinks(doc, info);
+            downloadImages(OVERWRITE, imagesLinks, PATH_STRING);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private static Document parseHtml (String url) throws IOException {
+        return Jsoup.connect(url).get();
+    }
+
+    private static Map<URI, URI> getImagesLinks (Document doc, WebsiteInfo info){
+            Map<URI, URI> links = new HashMap<>();
+            Elements imgs = doc.select(info.getTag());
 
             for (Element a : imgs) {
-                try {
-                    String[] linkParts = a.attr(TAG_ATTRIBUTE).split("/");
-                    String imageName = linkParts[linkParts.length - 1];
+                String[] linkParts = a.attr(info.getTagAttribute()).split("/");
+                String imageName = linkParts[linkParts.length - 1];
 
-                    String[] imageNameParts = imageName.split("\\.");
-                    String extension = imageNameParts[imageNameParts.length - 1];
+                String[] imageNameParts = imageName.split("\\.");
+                String extension = imageNameParts[imageNameParts.length - 1];
 
-                    if (imageNameParts.length > 1) {
-                        for (String imageExtension : ImageExtensions.getExtensions()) {
-                            if (imageExtension.matches(extension)) {
-                                {
-                                    Path path = Paths.get(PATH_STRING + "\\" + imageName);
-                                    URI u = URI.create(a.absUrl(TAG_ATTRIBUTE));
-                                    InputStream in = u.toURL().openStream();
-                                    if (OVERWRITE) {
-                                        Files.copy(in, path, StandardCopyOption.REPLACE_EXISTING);
-                                    } else Files.copy(in, path);
-                                    sb.append(imageName).append("\n");
-                                }
-                            }
+                if (imageNameParts.length > 1) {
+                    for (String imageExtension : ImageExtensions.getExtensions()) {
+                        if (imageExtension.matches(extension)) {
+                            links.put(URI.create(a.absUrl(TAG_ATTRIBUTE)), URI.create(imageName));
                         }
                     }
                 }
-                catch(FileNotFoundException ex) {
-                        System.out.println("Can't find the link " + a.absUrl(TAG_ATTRIBUTE));
+            }
+            return links;
+    }
+
+    private static void downloadImages (boolean overwrite, Map<URI, URI> imagesLinks, Path target) throws IOException {
+            for (Map.Entry<URI, URI> entry : imagesLinks.entrySet()) {
+                try {
+                    Path path = Paths.get(target + "\\" + entry.getValue());
+                    InputStream in = entry.getKey().toURL().openStream();
+                        if (overwrite) {
+                            Files.copy(in, path, StandardCopyOption.REPLACE_EXISTING);
+                        } else Files.copy(in, path);
+                } catch (FileNotFoundException ex) {
+                    System.out.println("Can't find the link " + entry.getKey());
+                } catch (FileAlreadyExistsException ex) {
+                    System.out.println("File " + entry.getValue() + " already exists!");
                 }
             }
         }
-        catch(IOException ex) {
-                ex.printStackTrace();
-            }
-            System.out.println(sb.toString());
-        }
     }
+
---
+++ GetImages/src/WebsiteInfo.java
@@ -0,0 +1,25 @@
+public class WebsiteInfo {
+
+private String url;
+private String tag;
+private String tagAttribute;
+
+    WebsiteInfo (String url, String tag, String tagAttribute) {
+        this.url = url;
+        this.tag = tag;
+        this.tagAttribute = tagAttribute;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public String getTagAttribute() {
+        return tagAttribute;
+    }
+
+}
